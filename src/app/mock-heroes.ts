5478791549b8d060f7ec9a263ff70aa22c8b32c6
chennan <ichennan@sina.com>
2018-04-08 17:23:09 +0800

    init

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 2, name: 'i am 2' },
+  { id: 3, name: 'i am 3' },
+  { id: 4, name: 'i am 4' },
+  { id: 5, name: 'i am 5' },
+  { id: 6, name: 'i am 6' },
+  { id: 7, name: 'i am 7' },
+  { id: 8, name: 'i am 8' },
+  { id: 9, name: 'i am 9' }
+]
